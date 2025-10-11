20739405e7128359d582bc66a02b095d87a1f3c4
Femi akinola <Obaoluwafemi.akinola@pau.edu.ng>
2025-10-11 19:28:14 +0100

    (My third commit)

---
+++ week-2/project/project_2.rs
@@ -0,0 +1,15 @@
+fn main () {
+	let a: f64 = 450_000.00;
+	let b: f64 = 1_500_000.00;
+	let c: f64 = 750_000.00;
+	let d: f64 = 2_850_000.00;
+	let e: f64 = 250_000.00;
+
+	// Sum
+	let sum = a + b +c + d + e;
+    println!("sum is {}", sum ); 
+    // average
+    let average = (a + b +c +d + e)/ 5.0;
+    println!("average is {}", average );
+
+}
